1b9041308997c19a8eaf7728a3725ac5aba99a92
Irmak <arabaciirmak@gmail.com>
2025-07-20 15:59:03 +0300

    commit1

--- obj/Debug/net6.0-windows/EquipmentListTable.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/EquipmentListTable.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EquipmentListTable")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c262a20b7afb1980583b2e8507aa6055f18b0f17")]
 [assembly: System.Reflection.AssemblyProductAttribute("EquipmentListTable")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EquipmentListTable")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
